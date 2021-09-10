f140fb8abcd41caa1fa405ed339ec44c1dfb851b
IshFire123 <89421371+IshFire123@users.noreply.github.com>
2021-09-09 19:35:07 -0700

    Update addRole.py

--- Pyroh/com/addRole.py
+++ Pyroh/com/addRole.py
@@ -1,21 +1,21 @@
-import discord
-from discord.ext import commands
+#import discord
+#from discord.ext import commands
 
-botClient = commands.Bot(command_prefix='!')
+#botClient = commands.Bot(command_prefix='!')
 
-@botClient.command()
-@commands.has_permissions(kick_members=True)
-async def addRole(ctx, member: discord.Member, *, role: discord.Role):
-    await member.add_roles(member, role)
-    await ctx.send(f'Role `{role}` has been added to `{member}`.')
+#@botClient.command()
+#@commands.has_permissions(kick_members=True)
+#async def addRole(ctx, member: discord.Member, *, role: discord.Role):
+   # await member.add_roles(member, role)
+   # await ctx.send(f'Role `{role}` has been added to `{member}`.')
 
 
-@botClient.command()
-@commands.has_permissions(kick_members=True)
-async def removeRole(ctx, member: discord.Member, *, role: discord.Role):
-    arole = discord.utils.get(ctx.message.server.roles, name=role)
-    if arole:
-        await member.remove_roles(member, role)
-        await ctx.send(f'Role `{role}` has been removed from `{member}`.')
-    else:
-        await ctx.send(f'`{member}` does not currently have the role `{role}`.')
+#@botClient.command()
+#@commands.has_permissions(kick_members=True)
+#async def removeRole(ctx, member: discord.Member, *, role: discord.Role):
+    #arole = discord.utils.get(ctx.message.server.roles, name=role)
+    #if arole:
+       # await member.remove_roles(member, role)
+     #   await ctx.send(f'Role `{role}` has been removed from `{member}`.')
+   # else:
+     #   await ctx.send(f'`{member}` does not currently have the role `{role}`.')
